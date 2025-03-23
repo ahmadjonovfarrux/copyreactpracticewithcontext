d3f9ed214dd6131cc5ed030958c93856f94076a7
Farrux <Farrux@gmail.com>
2025-03-23 13:47:01 +0500

    enhance ProductList and YourCart components with new buttons and improved styling

--- src/components/YourCart.jsx
+++ src/components/YourCart.jsx
@@ -1,5 +1,14 @@
 function YourCart() {
-  return <div className="yourCart">YourCart</div>;
+  return (
+    <div className="yourCart">
+      <h2 className="yourCart-title">Your Cart</h2>
+      <img
+        className="yourCart-img"
+        src="../public/images/illustration-empty-cart.svg"
+        alt="image of cart"
+      />
+    </div>
+  );
 }
 
 export default YourCart;
